eed03d4c6369128cf27f75fc44dc2cfc7dabfac5
edson_atonle <atonleedson@gmail.com>
2026-03-17 09:56:08 +0100

    cyber

--- llm/main.py
+++ llm/main.py
@@ -13,3 +13,17 @@ explanation = generate_explanation(prediction_data)
 
 print("\n===== EXPLICATION RH =====\n")
 print(explanation)
+
+try:
+    user_input = input("Entrez l'ID de l'employé : ")
+    
+    # 1. Vérification du format (Type checking)
+    employee_id = int(user_input) 
+    
+    # 2. Vérification de la valeur (Range checking)
+    if employee_id < 0 or employee_id > 99999:
+        raise ValueError("ID invalide")
+
+except ValueError:
+    print("Tentative d'injection ou format invalide détecté. Accès refusé.")
+    exit()
